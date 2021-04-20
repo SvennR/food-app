5c6ad78ac18b7309ccadcb8354b8793e417a7c4b
Svenn Rage <svennrage@gmail.com>
2021-04-20 16:21:12 +0200

    commit

---
+++ server/api/recipesFromApi.js
@@ -0,0 +1,8 @@
+var express = require('express');
+var RecipeFromWww = require('../models/recipesFromApi');
+
+var router = express.router();
+
+router.get('/:', function (req,res) {
+    
+});
---
+++ server/models/recipesFromApi.js
@@ -0,0 +1,23 @@
+/***  Use this as a template for API from future food-site team-work ***/
+
+/*
+const request = require('request-promise');
+
+const API_KEY = 'foodApp';
+
+class RecipeFromWww {
+    static retrieveBySite (recipe, callback) {
+        request({
+            uri: '' ,
+            json: true
+        }).then(function (res) {
+            callback(res);
+        }).catch(function (err) {
+            console.log(err);
+            callback({ error: 'Could not reach ... API.'})
+        });
+    }
+}
+
+module.exports = RecipeFromWww;
+*/
